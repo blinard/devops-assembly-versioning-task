129144f1e33b8f622601bca86875bce7bdb9f4f7
Brad Linard <brad.linard@idelata.com>
2019-10-13 21:55:47 -0400

    Removing unneeded files and packages

--- buildAndReleaseTask/tests/_suite.ts
+++ buildAndReleaseTask/tests/_suite.ts
@@ -120,21 +120,4 @@ describe('DevOps Assembly Versioning Task', function () {
 
         done();
     });
-
-    // it('it should fail if tool returns 1', function(done: MochaDone) {
-    //     this.timeout(1000);
-    
-    //     let tp = path.join(__dirname, 'failure.js');
-    //     let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-
-    //     tr.run();
-    //     console.log(tr.succeeded);
-    //     assert.equal(tr.succeeded, false, 'should have failed');
-    //     assert.equal(tr.warningIssues, 0, "should have no warnings");
-    //     assert.equal(tr.errorIssues.length, 1, "should have 1 error issue");
-    //     assert.equal(tr.errorIssues[0], 'Bad input was given', 'error issue output');
-    //     assert.equal(tr.stdout.indexOf('Hello bad'), -1, "Should not display Hello bad");
-
-    //     done();
-    // });
 });
